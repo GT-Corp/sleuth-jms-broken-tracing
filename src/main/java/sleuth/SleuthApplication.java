4dace73eac4a705ef33a214f130da1098fa70fd6
Ganesh Tiwari <ganesh.tiwari@shutterfly.com>
2020-08-04 21:57:00 -0500

    try 1

--- src/main/java/sleuth/SleuthApplication.java
+++ src/main/java/sleuth/SleuthApplication.java
@@ -1,19 +1,23 @@
 package sleuth;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.instrument.async.LazyTraceThreadPoolTaskExecutor;
 import org.springframework.context.annotation.*;
 import org.springframework.jms.annotation.*;
 import org.springframework.jms.config.*;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ErrorHandler;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.jms.*;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class SleuthApplication {
@@ -30,6 +34,7 @@ public class SleuthApplication {
 
         @Autowired RestTemplate restTemplate;
         @Autowired JmsTemplate jmsTemplate;
+        @Autowired Executor executor;
 
         @GetMapping("/test")
         void test() {
@@ -47,6 +52,8 @@ public class SleuthApplication {
             log.info("JMS message received {}", message.getText());
 
             restTemplate.getForEntity("http://localhost:8080/test", Void.class); //-->it works
+
+            executor.execute(() -> log.info("Im inside thread 2")); //it works
 
             throw new MyException("Some Error");  //-->it doesn't
         }
@@ -78,16 +85,30 @@ public class SleuthApplication {
 
         @Autowired ConnectionFactory connectionFactory;
 
+        @Autowired BeanFactory beanFactory;
+
         @Override
         public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
             registrar.setContainerFactory(containerFactory());
         }
 
+
         @Bean
-        JmsListenerContainerFactory<?> containerFactory() {
+        public Executor executor() {
+            ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+            threadPoolTaskExecutor.setCorePoolSize(1);
+            threadPoolTaskExecutor.setMaxPoolSize(1);
+            threadPoolTaskExecutor.initialize();
+
+            return new LazyTraceThreadPoolTaskExecutor(beanFactory, threadPoolTaskExecutor);
+        }
+
+        @Bean
+        JmsListenerContainerFactory<?> containerFactory( ) {
             DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
             factory.setConnectionFactory(connectionFactory);
             factory.setErrorHandler(jmsListenerErrorHandler);
+            factory.setTaskExecutor(executor());   //DOES NOT WORK !!!
             return factory;
         }
     }
